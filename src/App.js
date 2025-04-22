ab5ab5cf88efa2ab84e0d09819ecd6a805972f0b
reedonetech <reedone2022@gmail.com>
2025-04-22 10:07:35 -0700

    new

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './component/Nav';
 import Hero from './component/Hero';
+import AboutUs from './component/AboutUs';
+import Product from './component/Product';
+import Footer from './component/Footer';
 
 function App() {
   return (
     <div>
       <Nav />
       <Hero />
+      <AboutUs />
+      <Product />
+      <Footer/>
     </div>
   );
 }
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -36,12 +36,12 @@ const Hero = () => {
 
       {/* Content */}
       <div className='relative z-10 flex flex-col gap-5 justify-center items-center text-center w-full h-full px-4'>
-        <h1 className='text-[25px] lg:text-[35px] w-[35vw] font-bold text-white drop-shadow-lg'>
+        <h1 className='text-[25px] lg:text-[35px] lg:w-[35vw] font-bold text-white drop-shadow-lg'>
             Find quality service vendors and Get Your Work Done
             ...Effortlessly
         </h1>
         <div className='flex gap-[20px] lg:gap-[40px] pt-[10px] lg:pt-[20px]'>
-          <button className='bg-amber-600 text-white px-[35px] py-[10px] rounded hover:bg-inherit border-orange border-solid border-[3px] hover:text-black' onClick={handleEnrollClick}>
+          <button className='bg-black text-white px-[35px] py-[10px] rounded hover:bg-inherit border-orange border-solid border-[3px] hover:text-white' onClick={handleEnrollClick}>
             Get Started
           </button>
         </div>
